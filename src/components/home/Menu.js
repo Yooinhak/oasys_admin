60fcf2e41e81f5c42ef55350bd0d032f10150dea
Yooinhak <syu3236@gmail.com>
2022-07-29 23:50:06 +0900

    home-compoenet complete

--- src/components/home/Menu.js
+++ src/components/home/Menu.js
@@ -2,11 +2,38 @@ import React from 'react';
 import { BsPlusCircle } from 'react-icons/bs';
 import { ScrollMenu } from 'react-horizontal-scrolling-menu';
 
+function CategoriByMenu() {
+  return (
+    <div className='categori'>
+      <div className='item'>
+        <div className='item-img'>
+          <button />
+        </div>
+        <div className='item-info-box'>
+          <h3>메뉴 이름</h3>
+          <div>메뉴 설명</div>
+          <div>가격</div>
+        </div>
+      </div>
+      <div className='item'>
+        <div className='item-img'>
+          <button />
+        </div>
+        <div className='item-info-box'>
+          <h3>메뉴 이름</h3>
+          <div>메뉴 설명</div>
+          <div>가격</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 function Menu() {
   return (
     <div className='layout-menu'>
-      <h1>전체 메뉴</h1>
       <div className='section-categori'>
+        <h1>전체 메뉴</h1>
         <ScrollMenu>
           <button className='categori-btn'>찌개</button>
           <button className='categori-btn'>밥</button>
@@ -15,6 +42,10 @@ function Menu() {
             <BsPlusCircle size='30' />
           </button>
         </ScrollMenu>
+      </div>
+      <div className='section-menu-info'>
+        <CategoriByMenu />
+        <CategoriByMenu />
       </div>
     </div>
   );
